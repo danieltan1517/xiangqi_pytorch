7e5a670fccd8cf7eb79fef59c0ffc44a39bff5f2
Daniel Tan <danieltan@protonmail.com>
2023-08-22 14:50:08 -0700

    pandas removed.

--- pandas_example.py
+++
@@ -1,8 +0,0 @@
-import pandas
-import numpy
-
-dataframe = pandas.read_csv('text.txt', dtype={'eval':numpy.int16, 'positions':str})
-
-print(dataframe['eval'])
-for p in dataframe['positions']:
-  print(p)
